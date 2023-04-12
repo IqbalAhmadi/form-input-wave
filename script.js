dcaf1cd837f8e009aa972909951aa2a95a278c2e
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-04-12 13:51:26 -0700

    styled span classes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// variable
+const labels = document.querySelectorAll('.form-control label')
 
+// since it's node list we can use forEach to loop over
+labels.forEach((label) => {
+  label.innerHTML = label.innerText // this graps every letter
+    .split('')
+    .map((letter, idx) => `<span>${letter}</span>`)
+    .join('') // this turns it back to string
+})
